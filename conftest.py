a4a42a36171a0f0ba455450de120b00bd8445ec6
Ahmed Elsherif <gleader21@gmail.com>
2023-03-28 17:24:04 +0200

    addfinalizer

--- conftest.py
+++ conftest.py
@@ -4,11 +4,20 @@ import pytest
 environ["TESTING"] = str(True)
 
 
-@pytest.fixture()
+@pytest.fixture
 def db():
+    from app.database import SessionLocal
+    db = SessionLocal()
+    return db
+
+
+@pytest.fixture(autouse=True)
+def create_tables(db, request):
     from app.database import Base, engine, SessionLocal
 
     Base.metadata.create_all(engine)
-    db = SessionLocal()
-    yield db
-    Base.metadata.drop_all(engine)
+
+    def drop_tables():
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(drop_tables)
