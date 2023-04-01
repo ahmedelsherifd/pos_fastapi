9997dba44db7fd63ce11edfb369417dc23eccb25
Ahmed Elsherif <gleader21@gmail.com>
2023-04-01 07:29:30 +0200

    rollback

--- conftest.py
+++ conftest.py
@@ -1,22 +1,38 @@
 from os import environ
+from sqlalchemy.orm import Session
 import pytest
 
 environ["TESTING"] = str(True)
 
 
 @pytest.fixture
-def db():
-    from app.database import SessionLocal
-    db = SessionLocal()
-    return db
-
-
-@pytest.fixture(autouse=True)
-def database_setup_teardown(db, request):
+def db(request):
     from app.database import Base, engine
     Base.metadata.create_all(engine)
 
-    def drop_tables():
-        Base.metadata.drop_all(engine)
+    connection = engine.connect()
+    transaction = connection.begin()
 
-    request.addfinalizer(drop_tables)
+    db = Session(bind=connection)
+
+    def roll_back():
+        db.close()
+        transaction.rollback()
+        connection.close()
+
+    request.addfinalizer(roll_back)
+    return db
+
+
+# @pytest.fixture(autouse=True)
+# def database_setup_teardown(db: Session, request):
+#     from app.database import Base, engine
+#     Base.metadata.create_all(engine)
+
+#     def drop_tables():
+#         # Base.metadata.drop_all(engine)
+#         # roll back
+#         db.rollback()
+#         pass
+
+#     request.addfinalizer(drop_tables)
